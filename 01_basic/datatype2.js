49bc3be6d124ac770294e9185594fd329f69575f
suryanshbhati <144849456+suryanshbhati@users.noreply.github.com>
2026-03-23 12:29:31 +0000

    add initial datatype and variable examples

---
+++ 01_basic/datatype2.js
@@ -0,0 +1,16 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3 + 3); we are using nodejs, not browser
+
+// number => 2 ^ 53
+// bigInt
+// String => ""
+// boolean => true/false
+// null => standalone value
+// undefined => variable only declare yet not define
+// symbol => uniqueness
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
---
+++ 01_basic/variable1.js
@@ -0,0 +1,15 @@
+const accountId = 6348729
+let accountName = "John Doe"
+var accountPassword = "password123"
+accountCity = "New Delhi"
+let accountStatus
+
+accountName = "Surya bhati"
+accountPassword = "pasjjd234343"
+accountCity = "Jaipur"
+
+console.table([accountId, accountName, accountPassword, accountCity, accountStatus])
+
+/* 
+Prefer not to use var because of issue in block scope and functional scope.
+*/
