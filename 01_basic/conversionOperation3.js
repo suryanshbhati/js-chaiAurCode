eb2cc1f4ac6136b17d0046c26930b03a0fbd9522
suryanshbhati <144849456+suryanshbhati@users.noreply.github.com>
2026-03-23 13:02:25 +0000

    add conversion operations and type examples

---
+++ 01_basic/conversionOperation3.js
@@ -0,0 +1,29 @@
+let score = "33abc"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valInNum = Number(score);
+//console.log(typeof valInNum);
+//console.log(valInNum);
+
+// 33 => 33
+// "33abc" => NaN
+// true/false => 1/0
+// undefined => NaN
+// null => 0
+
+let isLogin = 1;
+
+let bIsLogin = Boolean(isLogin);
+//console.log(bIsLogin);
+
+// 1 => true, 0 => false
+// "" => false
+// "surya" => true
+
+let num = 10;
+let stringNum = String(num);
+console.log(typeof(stringNum));
+
+console.log(stringNum);
