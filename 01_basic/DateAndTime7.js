8b5e43bffe58768abb744dd9a167534b67c4e454
suryanshbhati <144849456+suryanshbhati@users.noreply.github.com>
2026-03-24 14:00:20 +0000

    add DateAndTime7.js with examples of date manipulation and timestamp generation

---
+++ 01_basic/DateAndTime7.js
@@ -0,0 +1,20 @@
+let myDate = new Date()
+//console.log(myDate.toString())
+//console.log(myDate.toDateString())
+//console.log(myDate.toLocaleString())
+
+//console.log(typeof(myDate))
+
+// let myCreatedDate = new Date(2026, 0, 23)
+//let myCreatedDate = new Date(2026, 0, 23, 5, 30)
+//let myCreatedDate = new Date("2026-01-18")
+let myCreatedDate = new Date("01-18-2026")
+//console.log(myCreatedDate.toLocaleString())
+
+let myTimeStamp = Date.now()
+//console.log(Math.floor(myTimeStamp / 1000))
+//console.log(myCreatedDate.getTime())
+
+let newDate = new Date()
+//console.log(newDate.getMonth() + 1)
+//newDate.toLocaleString("default", {weekday:"long"})
