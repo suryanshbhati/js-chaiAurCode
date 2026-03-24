bb981a0cf30bd5828fd84809e8d100627aecf08f
suryanshbhati <144849456+suryanshbhati@users.noreply.github.com>
2026-03-24 11:35:00 +0000

    add string5.js with initial string manipulation examples

---
+++ 01_basic/string5.js
@@ -0,0 +1,32 @@
+const name = "Surya"
+const cnt = 45
+
+//console.log(name + cnt)
+
+//console.log(`Hello my name is ${name} and my followers is ${cnt}`)
+
+const gameName = new String("Suryansh-Bhati-com-in")
+//console.log(gameName.split("-"))
+
+//console.log(gameName[0])
+//console.log(gameName.charAt(0))
+//console.log(gameName.__proto__)
+
+//console.log(gameName.length)
+//console.log(gameName.toUpperCase())
+
+//console.log(gameName.indexOf('a'))
+
+//const newStr = gameName.substring(0,4)
+//console.log(newStr)
+
+//const otherStr = gameName.slice(-13,-5)
+//console.log(otherStr)
+
+// const str = "    Surya Bhati    "
+// console.log(str)
+// console.log(str.trim())
+
+// const url = "https://surya.com/suryansh%20bhati"
+// console.log(url.replace("%20", "-"))
+// console.log(url.includes("surya"))
